fac040f5680f012baaed775e93ac17aaff3b8b7b
Nikki <nicleemart@gmail.com>
2016-10-13 20:21:06 -0500

    populate dropdown with array

--- php-query-string/calculator.php
+++ php-query-string/calculator.php
@@ -1,34 +1,25 @@
 <?php
-function calculator($firstNum, $operator, $secondNum)
-{
-    $operator = array("+", "-", "x");
-    $Submit = $_GET["Submit"];
 
-    if (isset($Submit) && ($name == "Nikki")) {
-        return "Nikki is the best name ever!";
-    } elseif (isset($Submit) && ($name == "Garnet")) {
-        return "I drink coffee for breakfast";
-    } elseif (isset($Submit) && ($name == "Amethyst")) {
-        return "Shorty Squad out.";
-    } elseif (isset($Submit) && ($name == "Pearl")) {
-        return "I like to watch you sleep sometimes. And by sometimes, I mean often.";
-    } elseif (isset($Submit) && ($name == "Steven")) {
-        return "No more cat fingers! I want Steven fingers! And I don't mean little me heads on fingers, I mean my regular fingers!";
-    } elseif (isset($Submit))  {
-        return "Oh, hey $name. Try a cooler name next time.";
-    }
+    $operator = array(
+        "add"=>"+",
+        "subtract"=>"-",
+        "multiply"=>"x"
+    );
+
+    if (!isset($_GET['submit'])) {
+
 } 
-    echo greeting($_GET["name"]); 
+    
 ?>
 
 <form method="get" action="">
 	<input type="text" name="firstNum" placeholder="Enter a number">
-    <select name="state" id="state">
-        <option value="" selected="selected">--- Select State ---</option>
-
-    </select>
     <select>
-      <option value="add" name="add">+</option>
+       <?php
+   foreach($operator as $key => $value){
+      echo '<option value="'.$key.'">'.$value.'</option>';
+  }
+ ?>
     </select>
     <input type="text" name="secondNum" placeholder="Enter a number">
 	<input type="submit" name="Submit">
